311b37b0a18ef9916c2e4a08c8118350a4840c32
Carl <cdba2019@mymail.pomona.edu>
2020-06-12 18:59:54 -1000

    release is gone

--- target/debug/wgpu-tutorial.d
+++
@@ -1,1 +0,0 @@
-/home/eumenes/rust-stuff/wgpu-tutorial/target/debug/wgpu-tutorial: /home/eumenes/rust-stuff/wgpu-tutorial/src/main.rs
--- target/debug/wgpu_tutorial-2071498f8be22560.d
+++
@@ -1,1 +0,0 @@
-/home/eumenes/rust-stuff/wgpu-tutorial/target/debug/wgpu_tutorial-2071498f8be22560: /home/eumenes/rust-stuff/wgpu-tutorial/src/main.rs
