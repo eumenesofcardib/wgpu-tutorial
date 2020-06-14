ffbe59365b698e67396d67a04aadcc3ce2a77b1a
Carl <cdba2019@mymail.pomona.edu>
2020-06-14 03:41:31 -1000

    Goblet board goblet board

--- src/main.rs
+++ src/main.rs
@@ -3,70 +3,135 @@ use winit::{
     event_loop::{EventLoop, ControlFlow},
     window::{Window, WindowBuilder},
 };
-
-fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .build(&event_loop)
-        .unwrap();
-
-    use futures::executor::block_on;
-
-    let mut state = block_on(State::new(&window));
-
-
-    event_loop.run(move |event, _, control_flow| {
-	    match event {
-		Event::WindowEvent {
-		    ref event,
-		    window_id,
-		} if window_id == window.id() => if !state.input(event) {
-		    match event {
-		        WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-		        WindowEvent::KeyboardInput {
-		            input,
-		            ..
-		        } => {
-		            match input {
-		                KeyboardInput {
-		                    state: ElementState::Pressed,
-		                    virtual_keycode: Some(VirtualKeyCode::Escape),
-		                    ..
-		                } => *control_flow = ControlFlow::Exit,
-		                _ => {}
-		            }
-		        }
-		        WindowEvent::Resized(physical_size) => {
-		            state.resize(*physical_size);
-		        }
-		        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-		            state.resize(**new_inner_size);
-		        }
-		        _ => {}
-		    }
-		}
-		Event::RedrawRequested(_) => {
-		    state.update();
-		    state.render();
-		}
-    Event::MainEventsCleared => {
-		    window.request_redraw();
-		}
-		_ => {}
-	    }
-    });
+extern crate fixedbitset;
+use fixedbitset::FixedBitSet;
+use math::round;
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
 }
+
+unsafe impl bytemuck::Pod for Vertex {}
+unsafe impl bytemuck::Zeroable for Vertex {}
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferDescriptor<'a> {
+        use std::mem;
+        wgpu::VertexBufferDescriptor {
+            stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::InputStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttributeDescriptor {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float3,
+                },
+                wgpu::VertexAttributeDescriptor {
+                    offset: mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float3,
+                },
+            ]
+        }
+    }
+}
+/*
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0],color: [0.5, 0.0, 0.5] }, // E
+];*/
+
+struct Goblet {
+		board: FixedBitSet,
+}
+
+impl Goblet {
+		fn new() -> Goblet {
+				let mut board = FixedBitSet::with_capacity(128);
+
+				Goblet {
+						board,
+				}
+		}
+		fn get_draw_data(&self) -> (Vec<Vertex>, Vec<u16>) {
+				let mut game_vertices: Vec<Vertex> = Vec::new();
+				let mut game_indices: Vec<u16> = Vec::new();
+				fn square_indices(starting_index: u16, indices: &mut Vec<u16>) {
+						indices.push((0+starting_index) as u16);
+						indices.push((1+starting_index) as u16);
+						indices.push((3+starting_index) as u16);
+
+						indices.push((1+starting_index) as u16);
+						indices.push((2+starting_index) as u16);
+						indices.push((3+starting_index) as u16);
+				}
+			//the platform
+				game_vertices.push(
+						Vertex { position: [-0.7, 0.7, 0.0], color: [0.5, 0.0, 0.5] });
+				game_vertices.push(
+						Vertex { position: [-0.7, -0.7, 0.0], color: [0.5, 0.0, 0.5] });
+				game_vertices.push(
+						Vertex { position: [0.7, -0.7, 0.0], color: [0.5, 0.0, 0.5] });
+				game_vertices.push(
+						Vertex { position: [0.7, 0.7, 0.0], color: [0.5, 0.0, 0.5] });
+				square_indices(0, &mut game_indices);
+
+				//squares
+				for i in 0..16 {
+						let x = (((((i % 4) + 1) as f32 * 0.04) + ((i % 4) as f32 * 0.3)) - 0.70) as f32;
+						let y = (0.70 - (((round::floor((i / 4) as f64, 0) + 1.0) * 0.04) +
+										 (round::floor((i / 4) as f64, 0) * 0.3))) as f32;
+						game_vertices.push(
+								Vertex { position: [x, y, 0.0], color: [0.0, 0.5, 0.0] });
+						game_vertices.push(
+								Vertex { position: [x, (y - 0.3), 0.0], color: [0.0, 0.5, 0.0] });
+						game_vertices.push(
+								Vertex { position: [(x + 0.3), (y - 0.3), 0.0], color: [0.0, 0.5, 0.0] });
+						game_vertices.push(
+								Vertex { position: [(x + 0.3), y, 0.0], color: [0.0, 0.5, 0.0] });
+						square_indices(((i + 1) * 4), &mut game_indices);
+				}
+				println!("{:?}", game_vertices);
+				println!("{:?}", game_indices);
+				(game_vertices, game_indices)
+		}
+}
+/*
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.7, 0.7, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.7, -0.7, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.7, -0.7, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.7, 0.7, 0.0], color: [0.5, 0.0, 0.5] }, // B
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 3,
+    1, 2, 3,
+/*    2, 3, 4,*/
+]; */
+/*
+const INDICES: &[u16] = &[
+		1, 2, 3,
+    0, 1, 2,
+];*/
 
 struct State {
     surface: wgpu::Surface,
-    adapter: wgpu::Adapter,
     device: wgpu::Device,
     queue: wgpu::Queue,
     sc_desc: wgpu::SwapChainDescriptor,
     swap_chain: wgpu::SwapChain,
 
-		clear_color: wgpu::Color,
-		
+    render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
+
     size: winit::dpi::PhysicalSize<u32>,
 }
 
@@ -81,16 +146,16 @@ impl State {
                 power_preference: wgpu::PowerPreference::Default,
                 compatible_surface: Some(&surface),
             },
-            wgpu::BackendBit::PRIMARY,
+            wgpu::BackendBit::PRIMARY, // Vulkan + Metal + DX12 + Browser WebGPU
         ).await.unwrap();
-        
+
         let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
             extensions: wgpu::Extensions {
                 anisotropic_filtering: false,
             },
             limits: Default::default(),
         }).await;
-        
+
         let sc_desc = wgpu::SwapChainDescriptor {
             usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
             format: wgpu::TextureFormat::Bgra8UnormSrgb,
@@ -100,27 +165,108 @@ impl State {
         };
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
-				let clear_color = wgpu::Color::BLACK;
+        let vs_src = include_str!("shader.vert");
+        let fs_src = include_str!("shader.frag");
+        let mut compiler = shaderc::Compiler::new().unwrap();
+        let vs_spirv = compiler.compile_into_spirv(vs_src, shaderc::ShaderKind::Vertex, "shader.vert", "main", None).unwrap();
+        let fs_spirv = compiler.compile_into_spirv(fs_src, shaderc::ShaderKind::Fragment, "shader.frag", "main", None).unwrap();
+        let vs_data = wgpu::read_spirv(std::io::Cursor::new(vs_spirv.as_binary_u8())).unwrap();
+        let fs_data = wgpu::read_spirv(std::io::Cursor::new(fs_spirv.as_binary_u8())).unwrap();
+        let vs_module = device.create_shader_module(&vs_data);
+        let fs_module = device.create_shader_module(&fs_data);
+
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            bind_group_layouts: &[],
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            layout: &render_pipeline_layout,
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: &vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::Back,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+            }),
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            color_states: &[
+                wgpu::ColorStateDescriptor {
+                    format: sc_desc.format,
+                    color_blend: wgpu::BlendDescriptor::REPLACE,
+                    alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                    write_mask: wgpu::ColorWrite::ALL,
+                },
+            ],
+            depth_stencil_state: None,
+            vertex_state: wgpu::VertexStateDescriptor {
+                index_format: wgpu::IndexFormat::Uint16,
+                vertex_buffers: &[
+                    Vertex::desc(),
+                ],
+            },
+            sample_count: 1,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+        });
+
+        /*let vertex_buffer = device.create_buffer_with_data(
+            bytemuck::cast_slice(VERTICES),
+            wgpu::BufferUsage::VERTEX,
+        );
+        let index_buffer = device.create_buffer_with_data(
+            bytemuck::cast_slice(INDICES),
+            wgpu::BufferUsage::INDEX,
+        );
+        
+        let num_indices = INDICES.len() as u32;*/
+
+				let game = Goblet::new();
 				
+				let (vertices, indices) = game.get_draw_data();
+				println!("\n\n\n{:?}", vertices.as_slice());
+				let vertex_buffer = device.create_buffer_with_data(
+            bytemuck::cast_slice(vertices.as_slice()),
+            wgpu::BufferUsage::VERTEX,
+        );
+        let index_buffer = device.create_buffer_with_data(
+            bytemuck::cast_slice(indices.as_slice()),
+            wgpu::BufferUsage::INDEX,
+        );
+        
+        let num_indices = indices.len() as u32;
+	
         Self {
             surface,
-            adapter,
             device,
             queue,
             sc_desc,
             swap_chain,
-
-						clear_color,
-						
+            render_pipeline,
+						vertex_buffer,
+            index_buffer,
+            num_indices,
             size,
         }
     }
 
+
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         self.size = new_size;
-    	self.sc_desc.width = new_size.width;
-    	self.sc_desc.height = new_size.height;
-    	self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
     }
 
     fn update(&mut self) {
@@ -128,45 +274,98 @@ impl State {
     }
 
     fn render(&mut self) {
-       let frame = self.swap_chain.get_next_texture()
-       	.expect("Timeout getting texture");
-       let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-       	label: Some("Render Encoder"),
-   	});
-   	{
-		let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-		    color_attachments: &[
-		        wgpu::RenderPassColorAttachmentDescriptor {
-		            attachment: &frame.view,
-		            resolve_target: None,
-		            load_op: wgpu::LoadOp::Clear,
-		            store_op: wgpu::StoreOp::Store,
-		            clear_color: self.clear_color,
-		        }
-		    ],
-		    depth_stencil_attachment: None,
-		});
-	}
+        let frame = self.swap_chain.get_next_texture()
+            .expect("Timeout getting texture");
 
-    	self.queue.submit(&[
-    	    encoder.finish()
-    	]);
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[
+                    wgpu::RenderPassColorAttachmentDescriptor {
+                        attachment: &frame.view,
+                        resolve_target: None,
+                        load_op: wgpu::LoadOp::Clear,
+                        store_op: wgpu::StoreOp::Store,
+                        clear_color: wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        },
+                    }
+                ],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(0, &self.vertex_buffer, 0, 0);
+            render_pass.set_index_buffer(&self.index_buffer, 0, 0);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
+        }
+
+        self.queue.submit(&[
+            encoder.finish()
+        ]);
     }
-		fn input(&mut self, event: &WindowEvent) -> bool {
-				match event {
-						WindowEvent::CursorMoved {
-								position,
-								..
-						} => {
-								self.clear_color = wgpu::Color {
-										r: position.x as f64 / self.size.width as f64,
-										g: position.y as f64 / self.size.height as f64,
-										b: 0.0,
-										a: 1.0,
-								};
-								true
-						}
-						_ => false,
-				}
-		}
 }
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .unwrap();
+
+    use futures::executor::block_on;
+
+    // Since main can't be async, we're going to need to block
+    let mut state = block_on(State::new(&window));
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::KeyboardInput {
+                        input,
+                        ..
+                    } => {
+                        match input {
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            _ => {}
+                        }
+                    }
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        // new_inner_size is &mut so w have to dereference it twice
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
+                }
+            }
+            Event::RedrawRequested(_) => {
+                state.update();
+                state.render();
+            }
+            Event::MainEventsCleared => {
+                // RedrawRequested will only trigger once, unless we manually
+                // request it.
+                window.request_redraw();
+            }
+            _ => {}
+        }
+    });
+}
+
+
